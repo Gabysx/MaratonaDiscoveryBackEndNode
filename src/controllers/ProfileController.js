b21ac5deab079dc7a02e300a3807a9b88817d46a
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-04-06 22:11:54 -0300

    update dp profile

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,36 +1,36 @@
-const Profile  = require('../model/Profile')
+const Profile = require('../model/Profile')
 
 module.exports = {
   index(req, res) {
-   return res.render("profile", { profile: Profile.get() })
+    return res.render("profile", { profile: Profile.get() })
   },
 
   update(req, res) {
 
-   // req.body para pegar os dados 
-   const data = req.body
+    // req.body para pegar os dados 
+    const data = req.body
 
-   // definir quantas semanas tem um ano = 52
-   const weeksPerYear = 52
+    // definir quantas semanas tem um ano = 52
+    const weeksPerYear = 52
 
-   // remover as semanas de férias do ano, para pegar quantas semanas tem em um mês 
-   const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
+    // remover as semanas de férias do ano, para pegar quantas semanas tem em um mês 
+    const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12
 
-   // quantas horas por semana estou trabalhando
-   const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+    // quantas horas por semana estou trabalhando
+    const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
 
-   // Total de horas trabalhadas no mes 
-   const monthlyTotalHours = weekTotalHours * weeksPerMonth
+    // Total de horas trabalhadas no mes 
+    const monthlyTotalHours = weekTotalHours * weeksPerMonth
 
-   //Qual vai ser o valor da minha hora 
-   const valueHour = data["monthly-budget"] / monthlyTotalHours
+    //Qual vai ser o valor da minha hora 
+    const valueHour = data["monthly-budget"] / monthlyTotalHours
 
-   Profile.get() = {
-     ...Profile.get(),
-     ...req.body,
-     "value-hour": valueHour,
-   }
+    Profile.update({
+      ...Profile.get(),
+      ...req.body,
+      "value-hour": valueHour,
+    })
 
-   return res.redirect('/profile')
+    return res.redirect('/profile')
   }
 }

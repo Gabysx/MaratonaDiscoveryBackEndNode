91e5ecd3ce2c900f9f69441b998ea6fa1df09875
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-04-06 21:30:45 -0300

    Colocação do caminho da views

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const server = express()
 const routes = require("./routes")
+const path = require("path")
 
 // usando o template engine
 server.set('view engine', 'ejs')
+
+//Mudar a localização da pasta views
+server.set('viwes', path.join(__dirname, 'views'))
 
 // Habilitar aquivos estaticos
 server.use(express.static("public"))

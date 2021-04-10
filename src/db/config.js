c7223df95c39b0a4a41a79a8f37421bd06d5e260
GABRIELA CANDIDO <67325201+Gabysx@users.noreply.github.com>
2021-04-10 16:00:49 -0300

    up

--- src/db/config.js
+++ src/db/config.js
@@ -2,10 +2,8 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
 // abrindo conexão com banco de dados 
-module.exports = () => {
+module.exports = () => 
   open({
     filename:'./database.sqlite',
     driver: sqlite3.Database
   });
-};
-
--- src/db/init.js
+++ src/db/init.js
@@ -1,16 +1,75 @@
-const Database =  require('config');
+const Database =  require('./config');
 
-Database();
+// async / await
 
-Database.exec(`CREATE TABLE profile (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  avatar TEXT,
-  monthy-budget INT,
-  days-per-week INT,
-  hours-per-day INT,
-  vacation-per-year INT,
-  value-hour INT,
-)`);
+const  initDB = {
+  async init(){
 
-Database.close();
+    const db = await Database();
+
+    await db.exec(`CREATE TABLE profile(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      avatar TEXT,
+      monthy_budget INT,
+      days_per_week INT,
+      hours_per_day INT,
+      vacation_per_year INT,
+      value_hour INT
+    )`);
+    
+    await db.exec(`CREATE TABLE jobs(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      daily_hours TEXT,
+      total_hours INT,
+      created_at DATETIME
+    )`);
+    
+    await db.run(`INSERT INTO profile(
+      name,
+      avatar,
+      monthy_budget,
+      days_per_week,
+      hours_per_day,
+      vacation_per_year,
+      value_hour
+    ) VALUES(
+      "Gabysx",
+      "https://github.com/gabysx.png",
+      3000,
+      5,
+      5,
+      3,
+      75
+    )`);
+    
+    await db.run(`INSERT INTO jobs(
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES(
+      "Pizzaria Guloso",
+      2,
+      1,
+      1617514376018
+    )`);
+    
+    await db.run(`INSERT INTO jobs(
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES(
+      "OneTwo Projects",
+      3,
+      47,
+      1617514376018
+    )`);
+    
+    await db.close();
+  }
+}
+
+initDB.init();
